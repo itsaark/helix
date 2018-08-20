99e3b2dd613055f5be17d449768e9a1bb3c30b89
Aark Koduru <aark@Aarks-MacBook-Pro.local>
2018-08-19 17:18:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Helix
+
+DNA is a very sensitive and personal information. I think that DNA sequencing, storage,
+and distribution should be completely anonymous.
+
+Helix is an attempt to create a blockchain to store and retrieve DNA anonymously.
+
+
+### Running
+Once you are inside the directory. `Cargo build` and `Cargo run` should start
+running the program.
+
+```
+Welcome to helix!
+Would you like to upload DNA? yes
+Please enter the DNA seq: ACTG
+Please enter your UID: 7yh
+Would you like to upload DNA? no
+Would you like to mine a block? yes
+Block has been mined successfully
+[Block { p_hash: "84D9C4B849506B6D8F8075A9000E7E0A254BE71060EA889FAD3C88395988F4FC", id: "550591AFB9D8F966212461930F37D6CFCC1DEA5EB32CACF0AD62A7C757BF4BBA", dna_hash: "E432B64B72068A1DCDDEA5622D599CC572F517865B5B918A0FAB0BADF78BAC72" }]
+
+```
+
+## Authors
+
+* **Aark Koduru**
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+* Thanks to Jeff for enlightening me about block chain.  
+* [PurpleBooth](https://github.com/PurpleBooth) for providing me a README template.
---
+++ src/main.rs
@@ -0,0 +1,190 @@
+// Copyright 2018 Aark Koduru
+//
+//!  Helix - Block chain for DNA storage and validation
+//!
+//!  Takes a string of DNA as input and when mined oututs
+//!  a the blockchain
+//!
+
+
+extern crate sha2;
+extern crate serde;
+extern crate serde_json;
+extern crate regex;
+extern crate hex;
+extern crate data_encoding;
+
+#[macro_use]
+extern crate serde_derive;
+
+use data_encoding::HEXUPPER;
+use std::io;
+use std::process;
+use std::io::Write;
+use sha2::{Digest, Sha512};
+use std::str;
+use regex::Regex;
+use serde_json::Error;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Block {
+    p_hash: String,
+    id: String,
+    dna_hash: String,
+}
+#[derive(Clone)]
+struct Txn {
+    id: String,
+    dna_hash: String,
+}
+#[derive(Clone)]
+struct Chain {
+    blockchain: Vec<Block>,
+    pendingTxns: Vec<Txn>,
+}
+
+impl Chain {
+    fn add_dna(&mut self, dna: String, uid: String){
+        let buf1 = sha2::Sha256::digest(dna.trim().as_bytes());
+        let dna_hash = HEXUPPER.encode(buf1.as_ref());
+
+        let buf2 = sha2::Sha256::digest(uid.trim().as_bytes());
+        let id = HEXUPPER.encode(buf2.as_ref());
+
+        let txn = Txn{
+                id,
+                dna_hash,
+        };
+        self.pendingTxns.push(txn);
+    }
+}
+
+/// This function takes in the the blockchain
+/// as an input and mines the first pending
+/// transcation
+fn mine_option(chain: &mut Chain) {
+    if (chain.pendingTxns.len() as i32) < 1{
+        println!("Currently there are no pending transcations available to mine.");
+        process::exit(0);
+    }
+
+    if(chain.blockchain.len() as i32) < 1 {
+        let mut dna_hash = chain.pendingTxns[0].dna_hash.clone();
+        let mut p_block = "0000000000";
+        let mut p_hash_buff = sha2::Sha256::digest(p_block.to_string().as_bytes());
+        let mut p_hash = HEXUPPER.encode(p_hash_buff.as_ref());
+        let mut id = chain.pendingTxns[0].id.clone();
+
+        let mut block = Block{
+            p_hash,
+            id,
+            dna_hash,
+        };
+        chain.blockchain.push(block);
+        chain.pendingTxns.remove(0);
+        println!("Block has been mined successfully");
+        println!("{:?}", chain.blockchain);
+        process::exit(0);
+    }else{
+        let mut dna_hash = chain.pendingTxns[0].dna_hash.clone();
+        for block in &chain.blockchain {
+            if dna_hash == block.dna_hash {
+                chain.pendingTxns.remove(0);
+                println!("You are trying to upload DNA which already exits on the blockchain");
+                process::exit(0);
+            }
+        }
+        let mut blockchain = chain.blockchain.clone();
+        let p_block = blockchain.last();
+        let p_block_json = serde_json::to_string(&p_block).unwrap();
+        let p_block_hash_buff = sha2::Sha256::digest(p_block_json.as_bytes());
+        let mut p_hash = HEXUPPER.encode(p_block_hash_buff.as_ref());
+        let mut id = chain.pendingTxns[0].id.clone();
+        let mut block = Block{
+            p_hash,
+            id,
+            dna_hash,
+        };
+        chain.blockchain.push(block);
+        chain.pendingTxns.remove(0);
+        println!("Block has been mined successfully");
+        println!("{:?}", chain.blockchain);
+        process::exit(0);
+    }
+
+}
+
+/// This function takes a string of DNA sequence
+/// as an input and adds it to the list of pending
+/// transcations
+fn upload_option(chain: &mut Chain) {
+    let mut dna = String::new();
+    let mut uid = String::new();
+    print!("Please enter the DNA seq: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut dna).expect("Failed To read Input");
+    if Regex::new(r"[^ACTGactg]").unwrap().is_match(&dna.trim()){
+        println!("You entered an invalid DNA");
+        process::exit(0);
+    }
+
+    print!("Please enter your UID: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut uid).expect("Failed To read Input");
+
+    chain.add_dna(dna, uid);
+
+}
+
+fn main() {
+
+    let mut user_response = String::new();
+    let mut blockchain = Vec::new();
+    let mut pendingTxns = Vec::new();
+
+    let mut chain = Chain{
+        blockchain,
+        pendingTxns,
+    };
+
+    println!("Welcome to helix!");
+
+    loop{
+
+        loop{
+            print!("Would you like to upload DNA? ");
+            io::stdout().flush();
+            user_response.clear();
+            io::stdin().read_line(&mut user_response).expect("Failed To read Input");
+            match user_response.to_lowercase().trim() {
+                "yes"|"y" => upload_option(&mut chain),
+                "no"| "n" => break,
+                _ => println!("Please enter yes/no"),
+            }
+        }
+
+        loop{
+            print!("Would you like to mine a block? ");
+            io::stdout().flush();
+            user_response.clear();
+            io::stdin().read_line(&mut user_response).expect("Failed To read Input");
+            match user_response.to_lowercase().trim() {
+                "yes"|"y" => mine_option(&mut chain),
+                "no"| "n" => break,
+                _ => println!("Please enter yes/no"),
+            }
+        }
+
+        print!("Would you like to exit? ");
+        io::stdout().flush();
+        user_response.clear();
+        io::stdin().read_line(&mut user_response).expect("Failed To read Input");
+        match user_response.to_lowercase().trim() {
+            "yes"|"y" => break,
+            "no"| "n" => continue,
+            _ => println!("Please enter yes/no"),
+        }
+
+    }
+
+}
